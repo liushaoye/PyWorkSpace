141b669964f163c92a770c88a90ca5891da19b0e
liuyang <shenshiyishao@163.com>
2018-11-04 20:54:18 +0800

    数组练习

---
+++ PythonProJ/2018week1/day005/001_continue.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+print('\n')
+print('**********continue exercise*************   \n')
+
+for i in range(10):
+	if (i < 5):
+		continue
+	print(i)
---
+++ PythonProJ/2018week1/day005/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/4 0004 0:50
+# @Author  : 北极的大企鹅！！
+# @FileName: __init__.py.py
+# @Software: PyCharm
