849ab53016297be1c2d2ca73461eb17150306809
liuyang <shenshiyishao@163.com>
2018-11-06 20:29:16 +0800

    元组

---
+++ PythonProJ/2018week1/day005/008_shop_demo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/6 0006 20:04
+# @Author  : 北极的大企鹅！！
+# @FileName: 008_shop_demo.py
+# @Software: PyCharm
+
+
+
+salary = int(input("请输入金额:"))
+
+
+shop_list=["1 iphone6s 5800","2 mac book 9000","","",""]
---
+++ PythonProJ/2018week1/day006/001_tuple_1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/6 0006 20:22
+# @Author  : 北极的大企鹅！！
+# @FileName: 001_tuple_1.py
+# @Software: PyCharm
+
+
+print('\n')
+print('**********元祖练习 *************   \n')
+
+
+####创建一个元组
+
+a=()
+
+print(a)
+
+print('\n')
+
+#### 元组赋值 ,元组不可以修改
+a=(22,)
+
+print(a)
+
+print('\n')
+
