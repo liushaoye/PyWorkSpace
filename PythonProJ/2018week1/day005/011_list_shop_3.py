232fbe5deddf27ded2ae1ece9c8795cbb27a8154
liuyang <shenshiyishao@163.com>
2018-11-09 09:39:34 +0800

    shop

---
+++ PythonProJ/2018week1/day005/011_list_shop_3.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/6 0006 20:04
+# @Author  : 北极的大企鹅！！
+# @FileName: 008_list_other_4.py
+# @Software: PyCharm
+
+
+print('\n')
+print('**********购物车编写 数组方式输出*************   \n')
+
+product_list = [
+	('Mac', 9000),
+	('Kindle', 800),
+	('Tesla', 900000),
+	('python book', 105),
+	('bike', 2000),
+]
+
+salary = input("请输入您的本金:")
+
+if salary.isdigit():
+	salary = int(salary)
+	for i,v in  enumerate(product_list,1) :
+		print(i,'----','品名',v[0],'----',"价格","$",v[1])
